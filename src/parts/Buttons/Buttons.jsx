d52220e27f33cd22ab17fbd094bb2efb2a232c39
Volodymyr <55vladimir5@gmail.com>
2024-09-21 10:52:47 +0200

    ,

--- src/parts/Buttons/Buttons.jsx
+++ src/parts/Buttons/Buttons.jsx
@@ -3,7 +3,7 @@ import styles from './Buttons.module.css';
 export default function Buttons () {
     return (
         <div className={styles.buttons}>
-            <p className={`waiting font-80 text-center margin-top-60 line-height-130`}>Ось так от пару проектів робимо <br></br>
+            <p className={`waiting font-80 text-center margin-top-60 line-height-130`}>Ось так от пару проектів робимо, <br></br>
             і все, на виліт</p>
         </div>
     );
